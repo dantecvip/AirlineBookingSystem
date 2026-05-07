6479f6327ff6cf9cb3bcb6f814c56703ce755276
Danilo Alves <danilo@samsoft.com.br>
2026-05-07 17:25:12 -0300

    Refactoring Flights Microservice to MongoDB using Cosmos DB for MongoDB

--- AirlineBookingSystem/AirlineBookingSystem.Flights.Infrastructure/Repositories/FlightRepository.cs
+++ AirlineBookingSystem/AirlineBookingSystem.Flights.Infrastructure/Repositories/FlightRepository.cs
@@ -1,33 +1,25 @@
 ﻿using AirlineBookingSystem.Flights.Core.Entities;
 using AirlineBookingSystem.Flights.Core.Repositories;
-using Dapper;
-using System.Data;
+using AirlineBookingSystem.Flights.Infrastructure.Data;
+using MongoDB.Driver;
 
 namespace AirlineBookingSystem.Flights.Infrastructure.Repositories
 {
-    public class FlightRepository(IDbConnection dbConnection) : IFlightRepository
+    public class FlightRepository(IFlightContext context) : IFlightRepository
     {
         public async Task AddFlightAsync(Flight flight)
         {
-            const string sql = @"
-                INSERT INTO Flights (Id, FlightNumber, Origin, Destination, DepartureTime, ArrivalTime)
-                VALUES (@Id, @FlightNumber, @Origin, @Destination, @DepartureTime, @ArrivalTime)";
-
-            await dbConnection.ExecuteAsync(sql, flight);
+            await context.Flights.InsertOneAsync(flight);
         }
 
         public async Task DeleteFlightAsync(Guid id)
         {
-            const string sql = "DELETE FROM Flights WHERE Id = @Id";
-
-            await dbConnection.ExecuteAsync(sql, new { Id = id });
+            await context.Flights.DeleteOneAsync(f => f.Id == id);
         }
 
         public async Task<IEnumerable<Flight>> GetFlightsAsync()
         {
-            const string sql = "SELECT * FROM Flights";
-
-            return await dbConnection.QueryAsync<Flight>(sql);
+            return await context.Flights.Find(flights => true).ToListAsync();
         }
     }
 }
